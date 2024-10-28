4a50dde63c0533c614b1129d7fcd595862854710
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-28 22:58:09 +0100

    update staff added

--- src/main/java/com/appsdevelopersblogs/spa/spa_example/FileStorageUtil.java
+++ src/main/java/com/appsdevelopersblogs/spa/spa_example/FileStorageUtil.java
@@ -1,10 +1,63 @@
 package com.appsdevelopersblogs.spa.spa_example;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Base64;
 
+import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 public class FileStorageUtil {
+
+    public static byte[] compressFileDeflater(byte[] file) {
+
+        Deflater deflater = new Deflater();
+        deflater.setLevel(Deflater.BEST_COMPRESSION);
+        deflater.setInput(file);
+        deflater.finish();
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream(file.length);
+        byte[] dataHolder = new byte[4 * 1024];
+
+        while (!deflater.finished()) {
+            int size = deflater.deflate(dataHolder);
+            stream.write(dataHolder, 0, size);
+        }
+
+        try {
+            stream.close();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return stream.toByteArray();
+    }
+
+    public static byte[] deCompressFileDeflater(byte[] file) throws DataFormatException {
+
+        Inflater inflater = new Inflater();
+        inflater.setInput(file);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream(file.length);
+        byte[] dataHolder = new byte[4 * 1024];
+
+        while (!inflater.finished()) {
+            int size = inflater.inflate(dataHolder);
+            stream.write(dataHolder, 0, size);
+        }
+
+        try {
+            stream.close();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return stream.toByteArray();
+    }
 
     public static byte[] compressFile(byte[] file) {
 
@@ -15,5 +68,17 @@ public class FileStorageUtil {
 
     public static byte[] deCompressFile(byte[] file) {
         return Base64.getDecoder().decode(file);
+    }
+
+    public static String toReadableString(byte[] bytes) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < bytes.length; i++) {
+            stringBuilder.append((char) bytes[i]);
+        }
+
+        return stringBuilder.toString();
+
     }
 }
---
+++ src/main/java/com/appsdevelopersblogs/spa/spa_example/TestInflaterAndDeflater.java
@@ -0,0 +1,46 @@
+package com.appsdevelopersblogs.spa.spa_example;
+
+import java.util.Base64;
+import java.util.zip.DataFormatException;
+
+public class TestInflaterAndDeflater {
+
+    public static void main(String[] args) throws DataFormatException {
+
+        StringBuilder content = new StringBuilder();
+
+        for (int i = 0; i < 10000; i++) {
+            content.append("abcdefghijklmnopqrstuvwxyz\n");
+        }
+
+        byte[] byteContent = content.toString().getBytes();
+        System.out.println("Actual size is: " + byteContent.length);
+        System.out.println();
+        System.out.println(content);
+
+
+        // Deflater
+        byte[] compressFileDeflater = FileStorageUtil.compressFileDeflater(byteContent);
+        byte[] deCompressFileDeflater = FileStorageUtil.deCompressFileDeflater(compressFileDeflater);
+
+        System.out.println("Deflater size is: " + compressFileDeflater.length);
+        System.out.println("Inflater size is: " + deCompressFileDeflater.length);
+        System.out.println();
+        System.out.println(FileStorageUtil.toReadableString(compressFileDeflater));
+        System.out.println();
+        System.out.println(FileStorageUtil.toReadableString(deCompressFileDeflater));
+
+        // Base64
+        byte[] base64Encoded = Base64.getEncoder().encode(byteContent);
+        byte[] decode = Base64.getDecoder().decode(base64Encoded);
+
+        System.out.println("Base64 encoded size is: " + base64Encoded.length);
+        System.out.println("Base64 decoded size is: " + decode.length);
+        System.out.println();
+        System.out.println(FileStorageUtil.toReadableString(base64Encoded));
+        System.out.println();
+        System.out.println(FileStorageUtil.toReadableString(decode));
+
+    }
+
+}
